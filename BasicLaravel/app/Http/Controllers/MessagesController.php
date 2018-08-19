7aa4191702b24182ff33523664036318580f1cf0
VincentCastig <vincent.castig@gmail.com>
2018-08-19 09:36:21 -0500

    Controllers and Validation

--- BasicLaravel/app/Http/Controllers/MessagesController.php
+++ BasicLaravel/app/Http/Controllers/MessagesController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    //
+    public function submit(Request $request){
+      $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required'
+      ]);
+
+      return 'SUCCESS';
+    }
 }
---
+++ BasicLaravel/resources/views/inc/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav class="site-header sticky-top py-1 navbar">
+  <div class="container d-flex flex-column flex-md-row justify-content-between">
+      <a class="py-2 d-none d-md-inline-block navbar-brand">Acme</a>
+    <a class="py-2 d-none d-md-inline-block" href="/">Home</a>
+    <a class="py-2 d-none d-md-inline-block" href="/about">About</a>
+    <a class="py-2 d-none d-md-inline-block" href="/contact">Contact</a>
+  </div>
+</nav>
