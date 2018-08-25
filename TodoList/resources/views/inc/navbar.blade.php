fcf89a30b738ca1a1e71d68e5f5d04276e0f1b05
VincentCastig <vincent.castig@gmail.com>
2018-08-25 09:09:51 -0500

    Todo Show Page and Includes

---
+++ TodoList/resources/views/inc/navbar.blade.php
@@ -0,0 +1,10 @@
+
+    <nav class="site-header sticky-top py-1 navbar">
+      <div class="container d-flex flex-column flex-md-row justify-content-between">
+          <a class="py-2 d-none d-md-inline-block navbar-brand" href="/">Home</a>
+        <a class="py-2 d-none d-md-inline-block" href="todo/create">Create Todo</a>
+      </div>
+    </nav>
+    
+    
+    
