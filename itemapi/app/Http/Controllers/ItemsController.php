ad9342e282b61e402284b102be61adc6ace5a2ea
VincentCastig <vincent.castig@gmail.com>
2018-08-26 12:31:00 -0500

    DELETE

--- itemapi/app/Http/Controllers/ItemsController.php
+++ itemapi/app/Http/Controllers/ItemsController.php
@@ -19,18 +19,9 @@ class ItemsController extends Controller
         return response()->json($items);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created resource in storage. 
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -68,17 +59,6 @@ class ItemsController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -87,7 +67,22 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $validator = Validator::make($request->all(),[
+        'text' => 'required'
+      ]);
+
+      if($validator->fails()){
+        $response = array('response' => $validator->messages(), 'success' => false);
+        return $response;
+      } else {
+        // Find an item
+        $item = Item::find($id);
+        $item->text = $request->input('text');
+        $item->body = $request->input('body');
+        $item->save();
+
+        return response()->json($item);
+      }
     }
 
     /**
@@ -98,6 +93,11 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+      // Find an item
+      $item = Item::find($id);
+      $item->delete();
+
+      $response = array('response' => 'Item deleted', 'success' => true);
+      return $response;
     }
 }
